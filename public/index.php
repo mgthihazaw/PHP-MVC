f9d1de82d45ba66816e4636cf147c03fbd010f1e
mgthihazaw <mgthihazaw777@gmail.com>
2020-01-29 16:05:57 +0630

    add url routing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "Hello PHP MVC";
+// echo 'QUERYSTRING = "' . $_SERVER['QUERY_STRING'] . '" <br>';
+// echo 'Requested URL = "' . $_SERVER['REQUEST_URI'] . '"';
+require '../Core/Router.php';
+
+$router = new Router();
+
+$router->add('/', ['controller' => 'Home', 'action' => 'index']);
+$router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('/posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+$url = $_SERVER['REQUEST_URI'];
+
+if ($router->match($url)) {
+    echo "<pre>";
+    var_dump($router->getParams());
+    echo "</pre>";
+} else {
+    echo "No route found for URL:{$url}";
+}
+// echo "<pre>";
+// var_dump($router->getRoutes());
+// echo "</pre>";
